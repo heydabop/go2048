5b90404e0a26380cc928c80f30607bb458baf125
heydabop <heydabop@gmail.com>
2014-05-06 00:37:35 -0500

    gofmt

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"image/color"
+	"log"
 
 	"github.com/BurntSushi/xgb/xproto"
 
@@ -13,35 +13,48 @@ import (
 
 var offWhite = xgraphics.BGRA{239, 248, 250, 255} //color of page background
 var bgGrey = xgraphics.BGRA{160, 173, 187, 255}   //color of board background
-var tile0 = xgraphics.BGRA{179, 192, 204, 255} //color of empty tile
-var tile2 = xgraphics.BGRA{218, 228, 238, 255} //color of 2 tile
-var tile4 = xgraphics.BGRA{200, 224, 237, 255} //4 tile
-var tile8 = xgraphics.BGRA{121, 177, 242, 255} //8 tile
-var tile16 = xgraphics.BGRA{99, 149, 245, 255} //16 tile
-var tile32 = xgraphics.BGRA{95, 124, 246, 255} //32 tile
-var tile64 = xgraphics.BGRA{59, 94, 246, 255} //64 tile
-var tile128 = xgraphics.BGRA{114, 207, 237, 255} //128 tile
-var tile256 = xgraphics.BGRA{97, 204, 237, 255} //256 tile
-var tile512 = xgraphics.BGRA{80, 200, 237, 255} //512 tile
-var tile1024 = xgraphics.BGRA{63, 197, 237, 255} //1024 tile
-var tile2048 = xgraphics.BGRA{46, 194, 237, 255} //2048 tile
-var tile4096 = xgraphics.BGRA{50, 58, 60, 255} //4096+ tile, i think 8192 and onward is also the same color
+var tile0 = xgraphics.BGRA{179, 192, 204, 255}    //color of empty tile
+var tile2 = xgraphics.BGRA{218, 228, 238, 255}    //color of 2 tile
+var tile4 = xgraphics.BGRA{200, 224, 237, 255}    //4 tile
+var tile8 = xgraphics.BGRA{121, 177, 242, 255}    //8 tile
+var tile16 = xgraphics.BGRA{99, 149, 245, 255}    //16 tile
+var tile32 = xgraphics.BGRA{95, 124, 246, 255}    //32 tile
+var tile64 = xgraphics.BGRA{59, 94, 246, 255}     //64 tile
+var tile128 = xgraphics.BGRA{114, 207, 237, 255}  //128 tile
+var tile256 = xgraphics.BGRA{97, 204, 237, 255}   //256 tile
+var tile512 = xgraphics.BGRA{80, 200, 237, 255}   //512 tile
+var tile1024 = xgraphics.BGRA{63, 197, 237, 255}  //1024 tile
+var tile2048 = xgraphics.BGRA{46, 194, 237, 255}  //2048 tile
+var tile4096 = xgraphics.BGRA{50, 58, 60, 255}    //4096+ tile, i think 8192 and onward is also the same color
 
 func colorNum(c color.Color) uint16 {
 	switch c {
-	case tile0: return 0
-	case tile2: return 2
-	case tile4: return 4
-	case tile8: return 8
-	case tile16: return 16
-	case tile32: return 32
-	case tile64: return 64
-	case tile128: return 128
-	case tile256: return 256
-	case tile512: return 512
-	case tile1024: return 1024
-	case tile2048: return 2048
-	case tile4096: return 4096
+	case tile0:
+		return 0
+	case tile2:
+		return 2
+	case tile4:
+		return 4
+	case tile8:
+		return 8
+	case tile16:
+		return 16
+	case tile32:
+		return 32
+	case tile64:
+		return 64
+	case tile128:
+		return 128
+	case tile256:
+		return 256
+	case tile512:
+		return 512
+	case tile1024:
+		return 1024
+	case tile2048:
+		return 2048
+	case tile4096:
+		return 4096
 	}
 	log.Panic("Unknown color!: ", c)
 	return 0
